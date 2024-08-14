b2b4f61967b37bab40e59e9487c4cedf8e6ce875
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2024-08-15 04:18:31 +0500

    STRINGS: PS1

---
+++ Strings/ps1.py
@@ -0,0 +1,17 @@
+# Print Twinkle Twinkle Little Start Poem
+
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!''')
+
+
+# ''' use to print multiple lines
