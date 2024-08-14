5a4075719ae16c2126d3b3e9b465632c4bd43ffc
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2024-08-15 04:08:40 +0500

    DataTypes: PS5

---
+++ DataTypes/ps5.py
@@ -0,0 +1,12 @@
+# Write a python program to find an average of two numbers entered by the user
+
+firstNum = input('Enter First Number')
+secNum = input('Enter Second Number')
+
+firstNumInt = int(firstNum)
+
+secNumInt = int(secNum)
+
+average = (firstNumInt + secNumInt) / 2
+
+print(average)
