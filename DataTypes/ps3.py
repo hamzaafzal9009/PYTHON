1e50a7080b24b0f67569dfef404a16e8052428d0
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2024-08-15 04:02:32 +0500

    DataType: PS3

---
+++ DataTypes/ps3.py
@@ -0,0 +1,7 @@
+# Check the type of variable assigned using input () function.
+
+userFeed = input('Enter Anything: ')
+
+userFeedType = type(userFeed)
+
+print("User input data type is: ", userFeedType)
